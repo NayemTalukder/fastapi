10eb38ccfa18c1ee97ca8c2e41c0430b69af4225
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-29 19:08:14 +0600

    Enhance Get Request

--- books.py
+++ books.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+BOOKS = {
+    'book_1': {'title': 'Title One', 'author': 'Author One'},
+    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
+    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
+    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
+    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
+}
+
+
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
+
+
 @app.get('/')
-async def first_api():
-  return {'message': 'hello World'}
+async def read_all_books():
+    return BOOKS
